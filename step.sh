e63abc12616deaeb9951aaa1233e917a7899c66a
Craig Hawco <craig.hawco@embrace.io>
2023-09-28 11:46:17 -0400

    Hook up dSYM upload

--- step.sh
+++ step.sh
@@ -1,22 +1,7 @@
 #!/bin/bash
 set -ex
 
-echo "This is the value specified for the input 'example_step_input': ${example_step_input}"
-
-#
-# --- Export Environment Variables for other Steps:
-# You can export Environment Variables for other Steps with
-#  envman, which is automatically installed by `bitrise setup`.
-# A very simple example:
-envman add --key EXAMPLE_STEP_OUTPUT --value 'the value you want to share'
-# Envman can handle piped inputs, which is useful if the text you want to
-# share is complex and you don't want to deal with proper bash escaping:
-#  cat file_with_complex_input | envman add --KEY EXAMPLE_STEP_OUTPUT
-# You can find more usage examples on envman's GitHub page
-#  at: https://github.com/bitrise-io/envman
-
-env 
-$(dirname "$0")/bin/macos/arm64/ios-upload --help
+$(BITRISE_STEP_SOURCE_DIR)/bin/macos/arm64/ios-upload --dsym ${BITRISE_DSYM_PATH}
 
 #
 # --- Exit codes:
