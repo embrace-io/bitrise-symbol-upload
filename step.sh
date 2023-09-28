3f87278c976b9d5538b0dad5ec3b1e9d1a5980a0
Craig Hawco <craig.hawco@embrace.io>
2023-09-28 16:00:27 -0400

    Clean up step shell script

--- step.sh
+++ step.sh
@@ -2,9 +2,3 @@
 set -ex
 
 ${BITRISE_STEP_SOURCE_DIR}/bin/macos/arm64/ios-upload --dsym ${BITRISE_DSYM_PATH}
-
-#
-# --- Exit codes:
-# The exit code of your Step is very important. If you return
-#  with a 0 exit code `bitrise` will register your Step as "successful".
-# Any non zero exit code will be registered as "failed" by `bitrise`.
