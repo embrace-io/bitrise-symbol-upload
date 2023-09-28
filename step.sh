d5ae9c564cc9aedc2b9741706a02a5275563bd48
Craig Hawco <craig.hawco@embrace.io>
2023-09-28 11:48:46 -0400

    Fix variable expansion

--- step.sh
+++ step.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ex
 
-$(BITRISE_STEP_SOURCE_DIR)/bin/macos/arm64/ios-upload --dsym ${BITRISE_DSYM_PATH}
+${BITRISE_STEP_SOURCE_DIR}/bin/macos/arm64/ios-upload --dsym ${BITRISE_DSYM_PATH}
 
 #
 # --- Exit codes:
