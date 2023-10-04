1d6d232d0273dd6ff3ae1f40b8f025e09719880b
Craig Hawco <craig.hawco@embrace.io>
2023-10-04 16:31:46 -0400

    Use lower case step input names.

--- step.sh
+++ step.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -ex
 
-${BITRISE_STEP_SOURCE_DIR}/bin/macos/$(uname -m)/ios-upload --dsym ${BITRISE_DSYM_PATH}
+EMBRACE_APP=${embrace_app} EMBRACE_API_TOKEN=${embrace_api_token} ${BITRISE_STEP_SOURCE_DIR}/bin/macos/$(uname -m)/ios-upload --dsym ${dsym_path}
