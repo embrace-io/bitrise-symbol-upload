aed3042794f28c1516e5feef8c017e1efa931dfb
Craig Hawco <craig.hawco@embrace.io>
2023-10-04 16:34:17 -0400

    Clean up env var usage.

--- step.sh
+++ step.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -ex
 
-EMBRACE_APP=${embrace_app} EMBRACE_API_TOKEN=${embrace_api_token} ${BITRISE_STEP_SOURCE_DIR}/bin/macos/$(uname -m)/ios-upload --dsym ${dsym_path}
+EMBRACE_APP=${embrace_app} EMBRACE_API_TOKEN=${embrace_api_token} $(dirname $0)/bin/macos/$(uname -m)/ios-upload --dsym ${dsym_path}
